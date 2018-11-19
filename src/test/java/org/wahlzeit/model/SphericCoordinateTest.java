e6a0c9511f6e4d0f9bac63f32c90f6c9200b8842
haegelix <haegelix@yahoo.de>
2018-11-19 12:47:05 +0100

    Adding test for spheric Coordinate

--- src/test/java/org/wahlzeit/model/SphericCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/SphericCoordinateTest.java
@@ -1,5 +1,15 @@
 package org.wahlzeit.model;
 
-public class SphericCoordinateTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+public class SphericCoordinateTest {
+	/**
+	 * Test if constructor checks for invalid values on init.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCoordinateInvalidValues() {
+		new CartesianCoordinate(Double.NaN, 1, 2);
+	}
 }
